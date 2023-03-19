278575c0edec517e2845697e25e1ca8d162bf20c
gimgihyeon <gihyeon6394@gmail.com>
2023-03-19 15:49:38 +0900

    시작

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 public class Main {
+
+
+    /**
+     *
+     * 파일에서 문자열 읽어오기
+     * 문자열에서 학교 명 찾아내기
+     *
+     *
+     *
+     *
+     * */
     public static void main(String[] args) {
         System.out.println("Hello world!11");
     }
 
+    public static
 
 }
